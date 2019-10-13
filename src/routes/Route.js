d62296cc4985605a6b2590d7dd4aa87877916672
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-13 17:37:17 -0300

    chore: private route

---
+++ src/routes/Route.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  path,
+  exact,
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route path={path} exact={exact} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  path: PropTypes.string.isRequired,
+  exact: PropTypes.bool,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+  exact: false,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
@@ -14,11 +15,16 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
 
-      <Route path="/meetup/register" exact component={MeetupRegister} />
-      <Route path="/meetup/details" exact component={MeetupDetails} />
+      <Route
+        path="/meetup/register"
+        exact
+        component={MeetupRegister}
+        isPrivate
+      />
+      <Route path="/meetup/details" exact component={MeetupDetails} isPrivate />
     </Switch>
   );
 }
