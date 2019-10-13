b8fbae6cdd6cc8f9c5ccd085e6d5d367e196aa9e
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-13 18:47:56 -0300

    chore: signin form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
   return (
-    <section>
-      <h1>SignIn</h1>
-    </section>
+    <>
+      <picture>
+        <img src={logo} alt="Meetapp" />
+      </picture>
+      <Form>
+        <Input name="email" type="email" placeholder="Your mail address" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">SignIn</button>
+        <Link to="/register">Create a free account</Link>
+      </Form>
+    </>
   );
 }
