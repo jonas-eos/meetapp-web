6fdb969145613ffc8c5287102b9eb84b4d5e160c
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-13 19:02:53 -0300

    chore: signup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
   return (
-    <section>
-      <h1>SignUp</h1>
-    </section>
+    <>
+      <picture>
+        <img src={logo} alt="Meetapp" />
+      </picture>
+      <Form>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your mail address" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">SignIn</button>
+        <Link to="/">Already registered</Link>
+      </Form>
+    </>
   );
 }
