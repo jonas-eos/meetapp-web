56ffdddde163af47aa96bfbb6fd96018a1e64997
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-14 22:56:09 -0300

    chore: pre-signin signup submit

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -5,12 +5,20 @@ import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  /**
+   * @function
+   * @param {object} data
+   */
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <picture>
         <img src={logo} alt="Meetapp" />
       </picture>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="Your mail address" />
         <Input name="password" type="password" placeholder="Password" />
