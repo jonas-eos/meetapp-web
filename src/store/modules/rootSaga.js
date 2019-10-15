0121b238005afc8ad4ff9c765a8acde16b6084ac
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-15 01:04:57 -0300

    fix rootSaga mistyping

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -7,6 +7,6 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 
-export default function* rootSata() {
+export default function* rootSaga() {
   return yield all([auth]);
 }
